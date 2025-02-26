a157cae6a6d6a4570eba27055a6e7ed64ceaea3a
ltlollo97 <63859259+ltlollo97@users.noreply.github.com>
2025-02-26 11:59:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,24 +7,16 @@ data "azurerm_resource_group" "example" {
   name = "PrismaCloudDemoEnv"
 }
 
+# Example resource using the existing resource group
 resource "azurerm_storage_account" "example" {
   name                     = "examplestorageacct"
   resource_group_name      = data.azurerm_resource_group.example.name
   location                 = data.azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
-  // Misconfiguration 1: High severity - Blob public access should be disabled
-  enable_blob_public_access = true
-
-  // Misconfiguration 2: Medium severity - Minimum TLS version should be set to 1.2
-  min_tls_version = "TLS1_0"
-
-  // Misconfiguration 3: Low severity - HTTPS traffic only should be enabled
-  enable_https_traffic_only = false
-
+  enable_https_traffic_only = false # misconfiguration
+  enable_blob_encryption = false # misconfiguration
   tags = {
-    Owner = "Lorenzo Nardi"
-    App   = "PrismaAppSecurity"
+    environment = "staging"
   }
 }
