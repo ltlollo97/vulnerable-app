35d804534f95181a8bb15af67efc3bd9e34f1be2
ltlollo97 <63859259+ltlollo97@users.noreply.github.com>
2025-02-26 12:04:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,14 @@ data "azurerm_resource_group" "example" {
 
 # Example resource using the existing resource group
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacct"
+  name                     = "test-storageacc-0"
   resource_group_name      = data.azurerm_resource_group.example.name
   location                 = data.azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  enable_https_traffic_only = false # misconfiguration
-  enable_blob_encryption = false # misconfiguration
+  enable_https_traffic_only = "false" # misconfiguration
+  enable_blob_encryption = "false" # misconfiguration
   tags = {
-    environment = "staging"
+    owner = "Lorenzo Nardi"
   }
 }
